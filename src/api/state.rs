c9ce05c0ac1b80986a550ca163dc1b3b92b3ae58
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-11-26 22:19:54 +0000

    feat: handlebars state

---
+++ src/api/state.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use handlebars::Handlebars;
+
+/// Tide state.
+#[derive(Clone)]
+pub struct State {
+    pub reg: Handlebars<'static>,
+}
+
+impl State {
+    /// Create a new [`State`] with the default templates.
+    pub fn with_default_templates() -> Result<State> {
+        let mut reg = Handlebars::new();
+
+        reg.register_template_file("index", "src/www/index.hbs")?;
+        reg.register_template_file("search", "src/www/search.hbs")?;
+
+        Ok(State { reg })
+    }
+}
