67c451d25fd04b5e660fe4ab731abb0ac9538bf0
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-12-02 20:59:25 +0000

    feat: display logo on website

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,22 @@ async fn main() -> Result<()> {
     let state = api::State::with_default_templates()?;
     let mut app = tide::with_state(state);
 
-    // Add routes
+    // Add function routes
     app.at("/").get(endpoints::index);
     app.at("/locations").get(endpoints::locations);
     app.at("/search").get(endpoints::search);
 
+    // Add static routes
+    app.at("common_style.css")
+        .serve_file("src/www/common_style.css")?;
     app.at("index_style.css")
         .serve_file("src/www/index_style.css")?;
     app.at("index_script.js")
         .serve_file("src/www/index_script.js")?;
     app.at("search_style.css")
         .serve_file("src/www/search_style.css")?;
+    app.at("logo.svg")
+        .serve_file("src/www/logo/vector/default-monochrome.svg")?;
 
     // Listen for connections
     app.listen("127.0.0.1:8080").await?;
