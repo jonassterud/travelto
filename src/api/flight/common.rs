97285a64f89b81ea91b09060181c6d14b63f0071
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-12-03 17:03:22 +0000

    feat: support iso8601 with no timezone

--- src/api/flight/common.rs
+++ src/api/flight/common.rs
@@ -1,22 +1,26 @@
 use anyhow::Result;
 
 /// Format duration as `"{}h {}m"`.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `seconds` - amount of seconds.
 pub fn format_duration(seconds: u64) -> String {
     format!("{}h {}m", (seconds / 60) / 60, (seconds / 60) % 60)
 }
 
 /// Format date as `"dd/mm, HH:MM"`.
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `date` - ISO 8601 date.
 pub fn format_date(date: &str) -> Result<String> {
-    Ok(date
-        .parse::<chrono::DateTime<chrono::Utc>>()?
-        .format("%d/%m, %R")
-        .to_string())
+    if let Ok(date_with_timezone) = date.parse::<chrono::DateTime<chrono::Utc>>() {
+        Ok(date_with_timezone.format("%d/%m, %R").to_string())
+    } else {
+        Ok(date
+            .parse::<chrono::NaiveDateTime>()?
+            .format("%d/%m, %R")
+            .to_string())
+    }
 }
