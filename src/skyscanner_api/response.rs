c953fe584e2ae0c7de372188260ce3921afb1b5e
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-12-03 00:42:17 +0000

    feat: response

---
+++ src/skyscanner_api/response.rs
@@ -0,0 +1,79 @@
+use serde::Deserialize;
+
+/// Response struct for search.
+#[derive(Debug, Deserialize)]
+pub struct SearchResponse {
+    /// Vector of itineraries.
+    pub itineraries: Vec<Itinerary>,
+    /// Context.
+    pub context: Context,
+}
+
+/// Itinerary
+#[derive(Debug, Deserialize)]
+pub struct Itinerary {
+    /// Vector of itinerary buckets.
+    pub buckets: Vec<ItineraryBucket>,
+}
+
+/// Itinerary bucket.
+#[derive(Debug, Deserialize)]
+pub struct ItineraryBucket {
+    /// Bucket id.
+    pub id: String,
+    /// Bucket name
+    pub name: String,
+    /// Bucket items.
+    pub items: Vec<BucketItem>,
+}
+
+/// Itinerary bucket item.
+#[derive(Debug, Deserialize)]
+pub struct BucketItem {
+    /// Price.
+    pub price: ItemPrice,
+    /// Legs.
+    pub legs: Vec<ItemLeg>,
+    /// Booking link.
+    pub deeplink: String,
+}
+
+/// Itinerary bucket item price.
+#[derive(Debug, Deserialize)]
+pub struct ItemPrice {
+    /// Price value.
+    pub raw: u32,
+}
+
+/// Itinerary bucket item leg.
+#[derive(Debug, Deserialize)]
+pub struct ItemLeg {
+    /// Origin location.
+    pub origin: Location,
+    /// Destination location.
+    pub destination: Location,
+    /// Duration in minutes.
+    #[serde(rename = "durationInMinutes")]
+    pub duration_mins: u32,
+    /// Departure date.
+    pub departure: String,
+    /// Arrival date.
+    pub arrival: String,
+}
+
+/// Location.
+#[derive(Debug, Deserialize)]
+pub struct Location {
+    /// Location id.
+    pub id: String,
+    /// Location name.
+    pub name: String,
+}
+
+
+/// Context.
+#[derive(Debug, Deserialize)]
+pub struct Context {
+    /// Status of search - `"incomplete"` or `"complete"`.
+    pub status: String,
+}
