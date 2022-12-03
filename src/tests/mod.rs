05db268dab52fb3882ad1d9de509454fc3651b27
Jonas Mathisrud Sterud <jonas.sterud@gmail.com>
2022-12-03 00:44:07 +0000

    feat: add test

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -3,9 +3,18 @@ use crate::*;
 /// Test [`api::get_flights`].
 #[test]
 fn test_api_get_flights() {
-    let config =
-        api::SearchConfig::new("NO", "DE", ("24/12/2022", "25/12/2022"), ("", ""), 1, 0, 0)
-            .unwrap();
+    let config = api::SearchConfig::new(
+        "NO".to_owned(),
+        "DE".to_owned(),
+        "24/12/2022".to_owned(),
+        Some("25/12/2022".to_owned()),
+        None,
+        None,
+        1,
+        0,
+        0,
+    )
+    .unwrap();
     api::get_flights(config).unwrap();
 }
 
@@ -26,8 +35,31 @@ fn test_kiwi_api_locations_query() {
 /// Test [`kiwi_api::search`].
 #[test]
 fn test_kiwi_api_search() {
-    let params =
-        kiwi_api::SearchParams::new("NO", "DE", "24/12/2022", "25/12/2022", "", "", 1, 0, 0)
-            .unwrap();
+    let params = kiwi_api::SearchParams::new(
+        "NO".to_owned(),
+        "DE".to_owned(),
+        "24/12/2022".to_owned(),
+        Some("25/12/2022".to_owned()),
+        None,
+        None,
+        1,
+        0,
+        0,
+    )
+    .unwrap();
     kiwi_api::search(params).unwrap();
+}
+
+/// Test [`skyscanner_api::search`].
+#[test]
+fn test_skyscanner_api_search() {
+    let params = skyscanner_api::SearchParams::new(
+        1,
+        "MUC".to_owned(),
+        "BER".to_owned(),
+        "2022/12/24".to_owned(),
+        Some("2022/12/25".to_owned()),
+    )
+    .unwrap();
+    dbg!(skyscanner_api::search(params).unwrap());
 }
